7fd4f184d05f877b588fcab1b3db0fc1b6a97c64
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-10-06 23:52:14 +0500

    Program 1

---
+++ program1.dart
@@ -0,0 +1,9 @@
+void main(){
+
+  print("--------------Program:1--------------");
+  List<String> names=["Sania Ali","Aemon Khan","Zara Ali","Saima Khan","Sara Khan"];
+  for(int i=0;i<names.length;i++){
+      print("Name:${names[i]}");
+  }
+
+}
